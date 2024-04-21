a3dff1b21f7ac2403872332ba2adc68bfd0e5abd
Jon F <Jon.fingas@gmail.com>
2024-04-21 11:33:39 -0500

    added input map and double jump

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -6,24 +6,30 @@ const JUMP_VELOCITY = -350.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var can_double_jump: bool = true
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 
-func _process(_delta):
-		update_animation()
+func _process(delta):
+	update_animation()
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		if Input.is_action_just_pressed("jump") and can_double_jump:
+			velocity.y = JUMP_VELOCITY
+			can_double_jump = false
+	else:
+		can_double_jump = true
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
